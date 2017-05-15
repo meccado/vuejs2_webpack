22dfc2c2424a4f2dd15758bcea1220fbe378b45b
“Moeketsi <moeketsielias.mokoena@investec.co.za>
2017-05-15 15:05:25 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+       app:  [
+           './src/app.js', 
+           './src/app.scss'
+       ],
+    } ,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    // devServer: {
+    //     contentBase: path.join(__dirname, "dist"),
+    //     compress: true,
+    //     port: 9000
+    // },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/, 
+                use: ['babel-loader'], 
+                // query: {
+                //     presets: ["es2015"]
+                // }
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'sass-loader'],
+                    publicPath: '/dist'
+                })
+            },
+            { 
+                test: /\.(png|jpe?g|gif|svg)$/, 
+                loaders: [
+                    {
+                        loader: 'file-loader', 
+                            options: {
+                                name: 'images/[name].[hash].[ext]'
+                            }
+                    },
+                    'img-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '', 'src', 'index.ejs'),
+            inject: 'body',
+            filename: 'index.ejs'
+        }),
+        new ExtractTextWebpackPlugin({
+            filename: '[name].[chunkhash].css',
+            disable: false,
+            allChunks: true
+        }),
+
+        function (){
+            this.plugin('done', stats => {
+                require('fs').writeFileSync(
+                    path.join(___dirname, 'dist/manifest.json'),
+                    JSON.stringify(stats.toJson().assetsByChunkName)
+                );
+            });
+        }
+        
+    ]   
+}
+
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    )
+}
